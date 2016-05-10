16a0e2a0546fe7df370b927ed8e5cfe5b9bfd2a9
Jon <berkowitz.jon@gmail.com>
2016-05-09 21:50:04 -0400

    initial logic for selected prev/next item with jquery - needs to be cleaned up and tied to clock

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 interval = null;
+var dict = {0: ["", "black"], 1: ["fa-heart","white"] , 2: ["fa-odnoklassniki", "orange"], 3: ["fa-apple","pink"], 4: ["fa-arrows-alt","blue"]
+	, 5: ["fa-chrome","red"], 6: ["fa-fort-awesome", "brown"], 7: ["fa-github-alt", "purple"], 8: ["fa-bus", "black"]};
+  var current = 1;
+
 
 $(document).ready(function() {
 	updateDate();
@@ -13,3 +17,31 @@ function updateDate(){
 	$("#hour").text(hour);
 	$("#minute").text(minute);
 }
+
+$( ".fa-arrow-circle-right" ).click(function() {
+	if($("#selected").hasClass(dict[current][0])) {
+		$("#selected").removeClass(dict[current][0]);
+	}
+	if (current < Object.keys(dict).length - 1) {
+		current = current + 1;
+	}
+	else {
+		current = 1;
+	}
+	$("#selected").addClass(dict[current][0]);
+	$("#selected").css("background-color",dict[current][1]);
+});
+
+$( ".fa-arrow-circle-left" ).click(function() {
+	if($("#selected").hasClass(dict[current][0])) {
+			$("#selected").removeClass(dict[current][0]);
+		}
+	if (current == 1) {
+		current = Object.keys(dict).length - 1;
+	}
+	else {
+		current = current - 1;
+	}
+	$("#selected").addClass(dict[current][0]);
+	$("#selected").css("background-color",dict[current][1]);
+});
